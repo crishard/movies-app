d0c8c940b1e087469c6557fe2d99e4dbc27f9a6c
crishard <crislantorres9@gmail.com>
2024-08-02 22:17:08 -0300

    criando página de detalhes dos filmes

---
+++ src/pages/Details.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MovieDetails } from '../components/Details';
+import IMovieDetails from '../Interfaces/IMovieInterface';
+import api from "../services/api";
+
+const Details = () => {
+    const [movie, setMovie] = useState<IMovieDetails>();
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getMovieDetails = async () => {
+            try {
+                const response = await api.get(`/3/movie/${id}`);
+                setMovie(response.data);
+            } catch (error) {
+                console.error("Error fetching movie details:", error);
+            }
+        };
+        getMovieDetails();
+    }, [id]);
+
+    if (!movie) return <p>Loading...</p>;
+
+    return (
+        <main className='flex justify-center items-center'>
+            <MovieDetails movie={movie} />
+        </main>
+    );
+};
+
+export default Details;
