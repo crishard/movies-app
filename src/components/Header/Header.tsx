95c7410dab00764e5815f74cfbfd112bfabd2963
crishard <crislantorres9@gmail.com>
2024-08-02 22:14:58 -0300

    criando navbar

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+import { NavBar } from "../NavBar";
+
+const Header = () => {
+
+    return (
+        <header className="text-gray-200 body-font pb-16">
+            <NavBar />
+            
+        </header>
+    )
+}
+
+export default Header
