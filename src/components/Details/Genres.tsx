ae1872ad9679cb15d4f4500a5ef01cfea6e1fad4
crishard <crislantorres9@gmail.com>
2024-08-02 22:16:20 -0300

    criando map de generos de filmes

---
+++ src/components/Details/Genres.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import IGender from '../../Interfaces/IGenderInterface';
+
+
+
+interface GenresProps {
+    genres:  IGender[];
+}
+
+const Genres: React.FC<GenresProps> = ({ genres }) => {
+    return (
+        <div className="pt-4 mx-auto w-[70%]">
+            <p className="flex items-center text-lg gap-2">
+                Gêneros:
+                <span className="flex gap-2">
+                    {genres.map((genre) => (
+                        <span key={genre.id}>{genre.name} |</span>
+                    ))}
+                </span>
+            </p>
+        </div>
+    );
+};
+
+export default Genres;
