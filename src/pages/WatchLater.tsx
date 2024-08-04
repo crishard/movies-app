8110e372eb0ebe3e380e29637f0037902669d755
crishard <crislantorres9@gmail.com>
2024-08-04 11:23:18 -0300

    criando página da lista para assisit mais tarde

--- src/pages/WatchLater.tsx
+++ src/pages/WatchLater.tsx
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import IMovieDetails from '../Interfaces/IMovieInterface';
+import { CardMovie } from '../components/WatchLater';
 
-const WatchLater = () => {
-  return (
-    <div>WatchLater</div>
-  )
-}
+const WatchLater: React.FC = () => {
+    const [movies, setMovies] = useState<IMovieDetails[]>([]);
 
-export default WatchLater
+    useEffect(() => {
+        const storedMovies = JSON.parse(localStorage.getItem('watchLaterMovies') || '[]');
+        setMovies(storedMovies);
+    }, []);
+
+    return (
+        <div className="watch-later min-h-[1100px]">
+            <h1 className="text-4xl text-gray-200 font-bold text-center py-4 pb-16">Lista para Assistir Mais Tarde</h1>
+            <div className="movie-list">
+                {movies.length > 0 ? (
+                    movies.map((movie) => (
+                        <CardMovie key={movie.id} movie={movie} />
+                    ))
+                ) : (
+                    <p className="text-center col-span-full">Nenhum filme na lista para assistir mais tarde.</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default WatchLater;
