dc8551be2dd642b71358580beb99e062f99f1383
crishard <crislantorres9@gmail.com>
2024-08-04 20:07:36 -0300

    adicionando filtro por gênero

--- src/components/List/ListMovies.tsx
+++ src/components/List/ListMovies.tsx
@@ -1,20 +1,28 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../css/Spinner.css";
 import IMovieDetails from "../../Interfaces/IMovieInterface";
 import api from "../../services/api";
 import MovieCard from './MovieCard';
-export const ListMovies = () => {
+
+interface IGenre {
+    id: number;
+    name: string;
+}
+
+export const ListMovies: React.FC = () => {
     const [movies, setMovies] = useState<IMovieDetails[]>([]);
+    const [genres, setGenres] = useState<IGenre[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
-
-    const getMovies = async (page: number) => {
+    const getMovies = async (page: number, genreId: number | null = null) => {
         try {
             setLoading(true);
-            const response = await api.get(`/3/discover/movie?page=${page}`);
+            const genreParam = genreId ? `&with_genres=${genreId}` : '';
+            const response = await api.get(`/3/discover/movie?page=${page}${genreParam}`);
             setMovies(prevMovies => page === 1 ? response.data.results : [...prevMovies, ...response.data.results]);
             setLoading(false);
         } catch (error) {
@@ -23,13 +31,23 @@ export const ListMovies = () => {
         }
     };
 
+    const getGenres = async () => {
+        try {
+            const response = await api.get('/3/genre/movie/list');
+            setGenres(response.data.genres);
+        } catch (error) {
+            console.error("Error fetching genres:", error);
+        }
+    };
+
     useEffect(() => {
+        getGenres();
         getMovies(1);
     }, []);
 
     useEffect(() => {
         if (page > 1) {
-            getMovies(page);
+            getMovies(page, selectedGenre);
         }
     }, [page]);
 
@@ -41,23 +59,40 @@ export const ListMovies = () => {
         setPage(prevPage => prevPage + 1);
     };
 
-    if (loading) {
+    const handleGenreChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const genreId = parseInt(event.target.value, 10);
+        setSelectedGenre(genreId);
+        setPage(1);
+        getMovies(1, genreId);
+    };
+
+    if (loading && page === 1) {
         return <div className="flex justify-center items-center mt-[10%]">
             <div className="spinner"></div>
         </div>
     }
 
-    if (!movies) {
-        return <p>Movie not found</p>;
-    }
-
     return (
         <div className="movie-list">
+             <div className="flex justify-end mb-10 ">
+                <div className="relative inline-block text-left ">
+                    <select
+                        className="w-full bg-transparent px-4 py-2 mt-1 text-base border border-gray-300 text-gray-200 rounded-md focus:outline-none overflow-auto focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        onChange={handleGenreChange}
+                        value={selectedGenre || ''}
+                    >
+                        <option value="" className="text-gray-900">Todos os Gêneros</option>
+                        {genres.map((genre) => (
+                            <option key={genre.id} value={genre.id} className="text-gray-900">{genre.name}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
             {movies.length > 0 ? (
                 <>
                     <ul className="grid lg:grid-cols-4 cursor-pointer md:grid-cols-3 gap-10 max-[770px]:grid-cols-2 max-[500px]:grid-cols-1 ">
                         {movies.map((movie) => (
-                            <MovieCard key={movie.id} movie={movie} onClick={handleCardClick} />
+                            <MovieCard key={movie.id} movie={movie} onClick={() => handleCardClick(movie.id)} />
                         ))}
                     </ul>
                     <div className="flex justify-center py-12">
