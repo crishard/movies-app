f694ee8565bf5e40f6c31be1073ca9b18883e1c1
crishard <crislantorres9@gmail.com>
2024-08-04 21:20:45 -0300

    adicionando funcionalidade de remover da lista

--- src/pages/WatchLater.tsx
+++ src/pages/WatchLater.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import IMovieDetails from '../Interfaces/IMovieInterface';
-import { CardMovie } from '../components/WatchLater';
+import CardMovie from '../components/WatchLater/CardMovie';
 
 const WatchLater: React.FC = () => {
     const [movies, setMovies] = useState<IMovieDetails[]>([]);
@@ -10,13 +11,26 @@ const WatchLater: React.FC = () => {
         setMovies(storedMovies);
     }, []);
 
+    const handleRemoveFromWatchLater = (movieId: number) => {
+        const updatedMovies = movies.filter(movie => movie.id !== movieId);
+        setMovies(updatedMovies);
+        localStorage.setItem('watchLaterMovies', JSON.stringify(updatedMovies));
+    };
+
     return (
         <div className="watch-later min-h-[1100px]">
             <h1 className="text-4xl text-gray-200 font-bold text-center py-4 pb-16">Lista para Assistir Mais Tarde</h1>
             <div className="movie-list">
                 {movies.length > 0 ? (
                     movies.map((movie) => (
-                        <CardMovie key={movie.id} movie={movie} />
+                        <CardMovie
+                            key={movie.id}
+                            movie={movie}
+                            onRemove={() => {
+                                handleRemoveFromWatchLater(movie.id);
+                                toast.success('Filme removido da lista para assistir mais tarde!');
+                            }}
+                        />
                     ))
                 ) : (
                     <p className="text-center col-span-full">Nenhum filme na lista para assistir mais tarde.</p>
