f2dacccb7458d2533a5ef33e921f7fcc6e942d14
crishard <crislantorres9@gmail.com>
2024-08-04 21:21:50 -0300

    adicionando filtro pela barra de pesquisa

--- src/components/List/ListMovies.tsx
+++ src/components/List/ListMovies.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../css/Spinner.css";
+import useDebounce from "../../hooks/UseDebounce";
 import IMovieDetails from "../../Interfaces/IMovieInterface";
 import api from "../../services/api";
 import MovieCard from './MovieCard';
@@ -16,7 +17,9 @@ export const ListMovies: React.FC = () => {
     const [selectedGenre, setSelectedGenre] = useState<number | null>(null);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState<string>('');
     const navigate = useNavigate();
+    const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
     const getMovies = async (page: number, genreId: number | null = null) => {
         try {
@@ -27,6 +30,18 @@ export const ListMovies: React.FC = () => {
             setLoading(false);
         } catch (error) {
             console.error("Error fetching movies:", error);
+            setLoading(false);
+        }
+    };
+
+    const searchMovies = async (searchTerm: string, page: number) => {
+        try {
+            setLoading(true);
+            const response = await api.get(`/3/search/movie?query=${searchTerm}&page=${page}`);
+            setMovies(prevMovies => page === 1 ? response.data.results : [...prevMovies, ...response.data.results]);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error searching movies:", error);
             setLoading(false);
         }
     };
@@ -47,9 +62,21 @@ export const ListMovies: React.FC = () => {
 
     useEffect(() => {
         if (page > 1) {
-            getMovies(page, selectedGenre);
+            if (debouncedSearchTerm) {
+                searchMovies(debouncedSearchTerm, page);
+            } else {
+                getMovies(page, selectedGenre);
+            }
         }
     }, [page]);
+
+    useEffect(() => {
+        if (debouncedSearchTerm) {
+            searchMovies(debouncedSearchTerm, 1);
+        } else {
+            getMovies(1, selectedGenre);
+        }
+    }, [debouncedSearchTerm, selectedGenre]);
 
     const handleCardClick = (movieId: number) => {
         navigate(`/movies/${movieId}`);
@@ -66,16 +93,25 @@ export const ListMovies: React.FC = () => {
         getMovies(1, genreId);
     };
 
-    if (loading && page === 1) {
-        return <div className="flex justify-center items-center mt-[10%]">
-            <div className="spinner"></div>
-        </div>
-    }
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = event.target.value;
+        setSearchTerm(searchTerm);
+    };
+
+
+
 
     return (
         <div className="movie-list">
-             <div className="flex justify-end mb-10 ">
-                <div className="relative inline-block text-left ">
+            <div className="flex justify-end mb-10">
+                <div className="relative inline-flex text-left gap-5">
+                    <input
+                        type="text"
+                        className="ml-4 p-2 border border-gray-300 rounded"
+                        placeholder="Pesquisar filmes"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
                     <select
                         className="w-full bg-transparent px-4 py-2 mt-1 text-base border border-gray-300 text-gray-200 rounded-md focus:outline-none overflow-auto focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         onChange={handleGenreChange}
@@ -87,8 +123,19 @@ export const ListMovies: React.FC = () => {
                         ))}
                     </select>
                 </div>
+
             </div>
-            {movies.length > 0 ? (
+
+
+            {
+                (loading && page === 1) && (
+                    <div className="flex justify-center items-center mt-[10%]">
+                        <div className="spinner"></div>
+                    </div>
+                )
+            }
+
+            {movies.length  >  0  ? (
                 <>
                     <ul className="grid lg:grid-cols-4 cursor-pointer md:grid-cols-3 gap-10 max-[770px]:grid-cols-2 max-[500px]:grid-cols-1 ">
                         {movies.map((movie) => (
