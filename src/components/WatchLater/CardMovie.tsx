067d8a1f52b9057da998b9a44dbebba747ea14f4
crishard <crislantorres9@gmail.com>
2024-08-04 21:21:33 -0300

    adicionando funcionalidade de remover da lista

--- src/components/WatchLater/CardMovie.tsx
+++ src/components/WatchLater/CardMovie.tsx
@@ -6,26 +6,36 @@ import StarRating from '../StarRatings/StarRatings';
 
 interface CardMovieProps {
     movie: IMovieDetails;
+    onRemove: () => void;
 }
 
-const CardMovie: React.FC<CardMovieProps> = ({ movie }) => {
+const CardMovie: React.FC<CardMovieProps> = ({ movie, onRemove }) => {
     const formattedDate = format(parseISO(movie.release_date), 'dd/MM/yyyy');
     return (
-        <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-card p-4 border  border-gray-200 rounded shadow block sm:flex w-full sm:h-[320px] py-4 mb-6 hover:scale-105 duration-500 cursor-pointer">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <div className='text-gray-200 px-10'>
-                <h2 className="text-2xl font-bold mt-2 sm:pb-8 pb-2">{movie.title}</h2>
-                <div className='text-xl font-medium'>
-                <p className='sm:pb-5 pb-2'>Data de lançamento: {formattedDate}</p>
-                <p className='md:line-clamp-4 sm:line-clamp-3 line-clamp-2'>{movie.overview}</p>
-                <div className='pt-5 mx-auto text-lg'>
-                Avaliação: <StarRating rating={movie.vote_average} />
-            </div>
-                </div>
-                
-            </div>
+        <div className=" p-4 border border-gray-200 rounded shadow sm:flex w-full py-4 mb-6">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='sm:h-[320px]' />
 
-        </Link>
+            <div className='text-gray-200 px-10'>
+                <Link to={`/movies/${movie.id}`} key={movie.id} className='hover:scale-105 duration-500 cursor-pointer'>
+                    <h2 className="text-2xl font-bold mt-2 sm:pb-8 pb-2">{movie.title}</h2>
+                    <div className='text-xl font-medium'>
+                        <p className='sm:pb-5 pb-2'>Data de lançamento: {formattedDate}</p>
+                        <p className='line-clamp-2'>{movie.overview}</p>
+
+                    </div>
+                </Link>
+                <div className='pt-5 mx-auto text-xl font-medium'>
+                    Avaliação: <StarRating rating={movie.vote_average} />
+                </div>
+
+                <button
+                    onClick={onRemove}
+                    className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+                >
+                    Remover
+                </button>
+            </div>
+        </div>
     );
 }
 
